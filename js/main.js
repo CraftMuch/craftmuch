636765c3a2146a4a213b22873c1fb4a77e15994c
BestCodrEver <70841501+BestCodrEver@users.noreply.github.com>
2020-11-14 10:21:50 +1100

    Added tawk.to

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5faf12bbc52f660e8973565b/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
+
 
 jQuery(function($) { // DOM is now read and ready to be manipulated
 equalheight = function(container){
